41c5a1fe554900c0377350a0439cca9b3994d417
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-06 14:09:44 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # {a: 1, b: 1}
-        count_s1 = Counter(s1)
-        count_win = defaultdict(int)
-        l = res = 0
-
+        if len(s1) > len(s2):
+            return False
+            
+        count_s1 = Counter(s1)  # s1 = 'ab', count_s1 = {a: 1, b: 1}
+        count_s2 = defaultdict(int) # sliding window
+        meet = l = 0 
+        # matched char in two dictionaries, count == len(count_s1) -> True
+        # after iterate through -> False
+        
         for r in range(len(s2)):
+            # add s2[r] into dict
             cur = s2[r]
-            count_win[cur] += 1
-            if count_win[cur] == count_s1[cur]:
-                res += 1
-
-            if r - l + 1 > len(s1):
+            count_s2[cur] += 1
+            if count_s2[cur] == count_s1[cur]:
+                meet += 1
+            if count_s2[cur] - 1 == count_s1[cur]:
+                meet -= 1 
+                
+            # check left pointer
+            if r - l + 1 > len(count_s1):
                 drop = s2[l]
-                if count_win[drop] == count_s1[drop]:
-                    res -= 1
-                count_win[drop] -= 1
+                if count_s2[drop] == count_s1[drop]:
+                    meet -= 1
+                count_s2[drop] -= 1
+                if count_s2[drop] == count_s1[drop]:
+                    meet +=1
                 l += 1
-
-            if res == len(count_s1):
+                
+            # check permutation
+            if meet == len(count_s1):
                 return True
-
+                
         return False
-
+                
+        
+                
+            
+                
+            
         
         
