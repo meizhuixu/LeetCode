9510dd86724e3db06da377d0543c11935c4112d4
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-05 21:25:29 -0800

    Time: 197 ms (36.21%), Space: 35.2 MB (38.41%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,31 +1,38 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # queue: big -> small
+        # left, right: 0-index
+        # increasing queue: big -> small
+        # step1: put nums[r] into queue tail
+        # step2: check sliding window is within k
+        # move l to right -> check queue[0] < l -> pop queue[0]
+        # step3: window == k -> add maximum(queue[0]) into res array
+        
+        # Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+        #                            l   r 
+        # queue = [] store index, not num
+        # time: O(n)
+        # space: O(k)
+        
         queue = deque()
         res = []
         l = 0
-
+        
         for r in range(len(nums)):
-            # add nums[r] into decreasing queue
+            # step1: add nums[r] into window
             while queue and nums[queue[-1]] < nums[r]:
                 queue.pop()
             queue.append(r)
-
-            # ensure the size of sliding window <= k
+            
+            # step2: check sliding window <= k
             if r - l + 1 > k:
                 l += 1
                 if queue[0] < l:
                     queue.popleft()
-
-            # add maximum into res when size == k
+                    
+            # step3: get maximum
             if r - l + 1 == k:
                 res.append(nums[queue[0]])
-
+                
         return res
-
-            
-
-
-
-
+                
         
