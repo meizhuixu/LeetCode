9f2f2fb1add37a3a1da399f427e9405250e204e1
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-06 13:48:47 -0800

    Time: 183 ms (56.51%), Space: 35.4 MB (32.11%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,38 +1,20 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # left, right: 0-index
-        # increasing queue: big -> small
-        # step1: put nums[r] into queue tail
-        # step2: check sliding window is within k
-        # move l to right -> check queue[0] < l -> pop queue[0]
-        # step3: window == k -> add maximum(queue[0]) into res array
-        
-        # Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
-        #                            l   r 
-        # queue = [] store index, not num
-        # time: O(n)
-        # space: O(k)
-        
-        queue = deque()
+        window = deque() 
         res = []
-        l = 0
-        
         for r in range(len(nums)):
-            # step1: add nums[r] into window
-            while queue and nums[queue[-1]] < nums[r]:
-                queue.pop()
-            queue.append(r)
-            
-            # step2: check sliding window <= k
-            if r - l + 1 > k:
-                l += 1
-                if queue[0] < l:
-                    queue.popleft()
-                    
-            # step3: get maximum
-            if r - l + 1 == k:
-                res.append(nums[queue[0]])
-                
+            cur = nums[r]
+            #remove the numbers smaller than the cur number from the right side
+            while window and nums[window[-1]] < cur:
+                window.pop()
+            window.append(r)
+            #popleft all the elements that are not in the sliding window
+            while window[0] < r - k + 1:
+                window.popleft()
+            #append the max num in the window to the res list 
+            if r >= k - 1:
+                res.append(nums[window[0]])
         return res
-                
+                 
+            
         
