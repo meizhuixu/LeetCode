1389c9feb143850ecce5417934be1429607aa44e
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-09 23:44:51 -0800

    Time: 167 ms (55.42%), Space: 20.8 MB (8.48%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r =  1, max(piles)
+
+        while l <= r:
+            mid = (l + r) // 2
+            
+            time = 0
+            for p in piles:
+                time += (p + mid - 1) // mid
+
+            if time <= h:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return l
+
+        
