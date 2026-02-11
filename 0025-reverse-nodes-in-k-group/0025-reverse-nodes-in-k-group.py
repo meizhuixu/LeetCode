bee279538ad928add689e62ab97612bdc197333c
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-11 15:37:14 -0800

    Time: 3 ms (34.14%), Space: 20.7 MB (12.21%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,25 +5,40 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if k == 1:
-            return head
-
-        length = 0
-        p = head
-        while p:
-            length += 1
-            p = p.next
-
-        pre = dummy = ListNode(next=head)
-        cur = head
-        for _ in range(length // k):
-            for _ in range(k - 1):
+        dummy = ListNode(0, head)
+        prev_g = dummy
+        #get the group of kth nodes
+            #if there is k nodes exits
+        #[dummy ,1, 2, 3, 4, 5] k = 2
+        # prev_g   kth
+        
+    
+        while True:
+            kth = self.getKth(prev_g, k)  
+            if not kth:
+                break    
+            #reverse the group of nodes
+            prev, cur = kth.next, prev_g.next
+            for _ in range(k):
                 temp = cur.next
-                cur.next = temp.next
-                temp.next = pre.next
-                pre.next = temp
-            pre = cur
-            cur = cur.next
-
+                cur.next = prev
+                prev = cur
+                cur = temp
+                
+            #dummy -> 2 -> 1 -> 3 -> 4 -> 5    
+            #prev_g  kth
+            #connect the group of nodes to the orignal linked list
+            temp1 = prev_g.next
+            prev_g.next = kth
+            prev_g = temp1
+        #return the head of the modified linked list
         return dummy.next
+    
+    def getKth(self, node, k):
+        cur = node
+        for _ in range(k):
+            if not cur.next:
+                return None
+            cur = cur.next
+        return cur
         
