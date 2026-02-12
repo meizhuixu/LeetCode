ee214adeeed8e90f594a21f3b489346c705ca66a
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-12 10:22:14 -0800

    Time: 0 ms (100%), Space: 20.3 MB (64.29%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,40 +5,28 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        prev_g = dummy
-        #get the group of kth nodes
-            #if there is k nodes exits
-        #[dummy ,1, 2, 3, 4, 5] k = 2
-        # prev_g   kth
-        
-    
-        while True:
-            kth = self.getKth(prev_g, k)  
-            if not kth:
-                break    
-            #reverse the group of nodes
-            prev, cur = kth.next, prev_g.next
-            for _ in range(k):
+        if k == 1:
+            return head
+            
+        # get the length of linked list
+        p = head
+        length = 0
+        while p:
+            length += 1
+            p = p.next
+            
+        # reverse k nodes
+        dummy = ListNode(next=head)
+        cur, prev = head, dummy
+        for _ in range(length // k):
+            for _ in range(k - 1):
+                # [dummy,|  1,  2, 3,| 4, 5, 6,| 7] k = 3
+                #    prev      cur temp
                 temp = cur.next
-                cur.next = prev
-                prev = cur
-                cur = temp
-                
-            #dummy -> 2 -> 1 -> 3 -> 4 -> 5    
-            #prev_g  kth
-            #connect the group of nodes to the orignal linked list
-            temp1 = prev_g.next
-            prev_g.next = kth
-            prev_g = temp1
-        #return the head of the modified linked list
-        return dummy.next
-    
-    def getKth(self, node, k):
-        cur = node
-        for _ in range(k):
-            if not cur.next:
-                return None
+                cur.next = temp.next
+                temp.next = prev.next
+                prev.next = temp
+            prev = cur
             cur = cur.next
-        return cur
-        
+            
+        return dummy.next
