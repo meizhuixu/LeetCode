c38ff01421d5923b2bbca2742b1e22bbd063d522
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-14 19:05:50 -0800

    Time: 2 ms (16.14%), Space: 19.5 MB (26.73%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,14 +1,13 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        pq = []
-        for stone in stones:
-            heapq.heappush(pq, -stone)
+        pq = [-s for s in stones]
+        heapq.heapify(pq)
 
         while len(pq) > 1:
-            x = -heapq.heappop(pq)
-            y = -heapq.heappop(pq)
-            if x > y:
-                heapq.heappush(pq, y - x)
+            x = heapq.heappop(pq)
+            y = heapq.heappop(pq)
+            if x < y:
+                heapq.heappush(pq, x - y)
 
         return -pq[0] if pq else 0
 
