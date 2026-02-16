65df8a21ed98d785294fa7528cd7a232e073bd30
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-15 21:15:32 -0800

    Time: 21 ms (25.54%), Space: 28.7 MB (22.23%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -13,20 +13,41 @@ class Twitter:
 
     def getNewsFeed(self, userId: int) -> List[int]:
         # add foloweeId + userId into one list
-        users = [user for user in self.following[userId]] + [userId]
+        users = list(self.following[userId]) + [userId]
+        users = self.following[userId] | {userId}
         
-        pq = []
+        # pq = []
+        # for user in users:
+        #     for time, tweet in self.tweets[user][-10:]:
+        #         heapq.heappush(pq, (time, tweet))
+        #         if len(pq) > 10:
+        #             heapq.heappop(pq)
+          #O(10*nlog10)          
+        # res = []
+        # while pq:
+        #     _, tweet = heapq.heappop(pq)
+        #     res.append(tweet)
+        #O(10log10)
+        # return res[::-1]
+
+        heap = []
         for user in users:
-            for time, tweet in self.tweets[user][-10:]:
-                heapq.heappush(pq, (time, tweet))
-                if len(pq) > 10:
-                    heapq.heappop(pq)
-                    
+            if self.tweets[user]:
+                idx = len(self.tweets[user]) - 1
+                time, t_id = self.tweets[user][idx]
+                heapq.heappush(heap, (-time, t_id, user, idx))
+        #O(nlogn)
         res = []
-        while pq:
-            _, tweet = heapq.heappop(pq)
-            res.append(tweet)
-        return res[::-1]
+        while heap and len(res) < 10:
+            t, t_id, user, idx = heapq.heappop(heap)
+            res.append(t_id)
+            if idx > 0:
+                idx -= 1
+                time, tweetId = self.tweets[user][idx]
+                heapq.heappush(heap, (-time, tweetId, user, idx))
+        return res
+        #O(10logn)  
+        #500
                  
 
     def follow(self, followerId: int, followeeId: int) -> None:
