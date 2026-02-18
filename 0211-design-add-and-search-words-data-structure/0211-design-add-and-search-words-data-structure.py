6464caea01b294c7691c97331327ff632f4a7bb7
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-18 15:24:34 -0800

    Time: 1230 ms (31.26%), Space: 68.5 MB (34.6%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,8 +1,8 @@
 class TrieNode:
-
     def __init__(self):
-        self.children = {}
+        self.children = {} # {chr: TrieNode}
         self.is_end = False
+
 
 class WordDictionary:
 
@@ -11,34 +11,33 @@ class WordDictionary:
         
 
     def addWord(self, word: str) -> None:
-        node = self.root
+        # 'bad'
+        node = self.root # start from root
         for chr in word:
             if chr not in node.children:
                 node.children[chr] = TrieNode()
             node = node.children[chr]
-
-        node.is_end = True
+        node.is_end = True # mark a word ending  
         
 
     def search(self, word: str) -> bool:
         def dfs(node, idx):
-            if idx >= len(word):
+            if idx == len(word):
                 return node.is_end
-
+                
             chr = word[idx]
             if chr == '.':
-                for child in node.children.values():
-                    if dfs(child, idx + 1):
+                for next_node in node.children.values():
+                    if dfs(next_node, idx + 1):
                         return True
                 return False
             else:
                 if chr not in node.children:
                     return False
                 return dfs(node.children[chr], idx + 1)
-
-        return dfs(self.root, 0)
-        
                 
+        return dfs(self.root, 0)
+                    
         
 
 
