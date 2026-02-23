c5773b5900bcc3792c272eac39b2ed5a8792016a
Meizhui Xu <xu.meiz@northeastern.edu>
2026-02-23 14:53:21 -0800

    Time: 7 ms (50.62%), Space: 20.7 MB (36.68%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,20 +1,23 @@
+# add count to record the number of components
+# initiate count as the number of nodes
+# whenever join two nodes(edge), count - 1
+# after join all the nodes by edges, return count
 class UnionFind:
     def __init__(self, n):
-        self.parent = [i for i in range(n)]
+        self.parent = [i for i in range(n)]  # idx: node  val: its parent
         self.count = n
-
+        
     def find(self, u):
         if self.parent[u] == u:
             return u
-        else:
-            self.parent[u] = self.find(self.parent[u])
-            return self.parent[u]
-
-    def isSame(self, u, v):
+        self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+        
+    def isSame(self, u, v):  # No need here
         u = self.find(u)
         v = self.find(v)
         return u == v
-
+        
     def join(self, u, v):
         u = self.find(u)
         v = self.find(v)
@@ -23,11 +26,13 @@ class UnionFind:
         self.parent[u] = v
         self.count -= 1
 
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         uf = UnionFind(n)
         for u, v in edges:
             uf.join(u, v)
-
         return uf.count
+            
+        
         
