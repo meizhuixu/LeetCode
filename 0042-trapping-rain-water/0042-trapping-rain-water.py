4941d61468b45691f6d4845a186c18305aad582e
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-05 18:33:18 -0800

    Time: 11 ms (47.65%), Space: 20.9 MB (93.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,14 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # keep track of the maximum height seen so far from both the left and the right sides
         max_left = max_right = 0
+        # placing pointers at both ends
+        # initialize l at the beginning and r at the end of the array
         l, r = 0, len(height) - 1
-        max_left, max_right
         res = 0
 
         while l <= r:
+            # The water level at any point is determined by the shorter of the two tall bars on its left and right
+            # process the side that has the smaller maximum
             if max_left < max_right:
+                # If we are moving the left pointer, we first update max_left
                 max_left = max(max_left, height[l])
+                # If the current bar is shorter than max_left, the difference is the trapped water at this specific column
                 res += max_left - height[l]
+                # moving inwards / moving the pointer towards the center
                 l += 1
             else:
                 max_right = max(max_right, height[r])
@@ -16,6 +23,7 @@ class Solution:
                 r -= 1
 
         return res
+        # It only takes O(n) time because we traverse the array once, and O(1) space since we only use a few variables
 
 
 
