160c0e1bec501e6f04543e9424bcbd513132b465
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-05 18:10:20 -0800

    Time: 7 ms (75.54%), Space: 21.4 MB (7.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,14 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # initialize an empty stack to store the indices of the bars
         stack = []
+        # and a variable res to accumulate the total amount of water trapped
         res = 0
 
+        # iterate through the array
         for i in range(len(height)):
+            # if the current bar is taller than the stack top, we've found a right boundary
             while stack and height[i] > height[stack[-1]]:
+                # the bottom of the valley
                 cur = stack.pop()
+                # If the stack is not empty, the new stack top is our left boundary
                 if stack:
+                    # calculate the trapped water for this horizontal layer.
+                    # the bounded height is the distance between the shorter of the two boundaries and the floor/the height is determined by the shorter of the two boundaries minus our floor
+                    # the width is the distance between the boundaries
                     res += (min(height[stack[-1]], height[i]) - height[cur]) * (i - stack[-1] - 1)
-
+            # maintain a strictly decreasing stack
             stack.append(i)
 
         return res
