a8083628f6a143b22fa4d60f6da2c192e8c81347
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-07 12:59:21 -0800

    Time: 59 ms (86.05%), Space: 19.7 MB (71.88%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,32 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
-            return ''
-
-        dict_t = Counter(t)
-        need = len(dict_t)
-
-        dict_win = defaultdict(int)
-        l = meet = 0
-        res = (0, float('inf'))
-
+        res = (-1, float('inf'))
+        l = match = 0
+        need = Counter(t)
+        window = defaultdict(int)
+        
         for r in range(len(s)):
-            cur = s[r]
-            dict_win[cur] += 1
-            if dict_win[cur] == dict_t[cur]:
-                meet += 1
+            if s[r] in need:
+                window[s[r]] += 1
+                if window[s[r]] == need[s[r]]:
+                    match += 1
 
-            while meet == need:
+            while match == len(need):
                 if r - l + 1 < res[1]:
                     res = (l, r - l + 1)
-
-                drop = s[l]
-                if dict_win[drop] == dict_t[drop]:
-                    meet -= 1
-                dict_win[drop] -= 1
+                if s[l] in window:
+                    window[s[l]] -= 1
+                    if window[s[l]] < need[s[l]]:
+                        match -= 1
                 l += 1
 
-        return '' if res[1] == float('inf') else s[res[0]: res[0] + res[1]]
+        return s[res[0]: res[0] + res[1]] if res[0] != -1 else ''
 
+
+            
 
         
