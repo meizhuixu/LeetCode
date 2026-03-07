6af4c610f3ef2bcd283e8c20ce61880e7a99947d
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-07 13:18:47 -0800

    Time: 198 ms (41.16%), Space: 35.3 MB (44.58%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,16 +1,26 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        queue = deque()
-        l = 0
+        # maintain a decreasing monotonic queue
+        # add new number into right (min), pop all the smaller number
+        # pop a number from left(max)
+        # nums = [1, 3,-1,-3,5,3,6,7], k = 3
+        #                      l   r
+        # queue = [7]
+        # res = [3, 3, 5, 5, 6, 7]
+        # time: O(n) n = len(nums)
+        # space: O(k) 
 
+        res = []
+        l = 0
+        queue = deque()
+        
         for r in range(len(nums)):
-            while queue and nums[queue[-1]] <= nums[r]:
+            while queue and nums[queue[-1]] < nums[r]:
                 queue.pop()
             queue.append(r)
 
             if r - l + 1 > k:
-                if queue[0] <= l:
+                if l == queue[0]:
                     queue.popleft()
                 l += 1
 
@@ -18,6 +28,4 @@ class Solution:
                 res.append(nums[queue[0]])
 
         return res
-
-
-
+        
