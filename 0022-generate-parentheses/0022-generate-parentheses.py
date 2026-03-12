c82e74497b592afa5382860add3b58cc2a5c46eb
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-11 21:16:55 -0700

    Time: 0 ms (100%), Space: 19.5 MB (67.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,15 +1,26 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+
+        # n = 3
+        # ((())) -> l <= n
+        # )  -> r <= l
+
         res = []
 
         def backtracking(l, r, path):
+            # path store generated paren, array
+            # base case
             if l == r == n:
                 res.append(''.join(path))
+                return
 
+            # add left
             if l < n:
                 path.append('(')
                 backtracking(l+1, r, path)
                 path.pop()
+
+            # add right
             if r < l:
                 path.append(')')
                 backtracking(l, r+1, path)
@@ -17,4 +28,8 @@ class Solution:
 
         backtracking(0, 0, [])
         return res
+
+
+
+
         
