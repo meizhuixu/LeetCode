afeba7fdc0508bd804466ba9a1b2b981f6602ceb
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-15 14:34:55 -0700

    Time: 0 ms (100%), Space: 20.7 MB (18.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        idx_list = [(num, i) for i, num in enumerate(nums)]
+        hashmap = defaultdict(int)
 
-        idx_list.sort()
-        l, r = 0, len(idx_list) - 1
-        while l < r:
-            total = idx_list[l][0] + idx_list[r][0]
-            if total == target:
-                return [idx_list[l][1], idx_list[r][1]]
-            elif total > target:
-                r -= 1
-            else:
-                l += 1
+        for i, num in enumerate(nums):
+            if num in hashmap:
+                return [i, hashmap[num]]
+            hashmap[target - num] = i
 
         return
 
