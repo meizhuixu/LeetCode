8dcfee0e5796716b1debbeff4521c7e60df639c0
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-15 14:52:06 -0700

    Time: 16 ms (22.3%), Space: 21.4 MB (11.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,31 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # two pointers
-        # calculate each colomn, smaller of left and right tall bars
-        # update the max_left and max_right
-        # max_left(bottleneck) < max_right: max_left & cur -> difference
-        # pointers move inward
-        # time: O(n)
-        # space: O(1)
-        # max_left = 0, max_right = 1
-        
-        # height = [2, 1, 2]
-        #             l                       r 
-        l, r = 0, len(height) - 1
-        max_left, max_right = height[l], height[r]
+        stack = []
         res = 0
-        
-        # iterate through the array
-        while l <= r:
-            if max_left < max_right:
-                # height of water is determined by max_left
-                max_left = max(max_left, height[l])
-                res += max_left - height[l]
-                l += 1
-            else:
-                # height of water is determined by max_right
-                max_right = max(max_right, height[r])
-                res += max_right - height[r]
-                r -= 1
-                
+
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                cur = stack.pop()
+                if stack:
+                    res += (min(height[i], height[stack[-1]]) - height[cur]) * (i - stack[-1] - 1)
+
+            stack.append(i)
+
         return res
+
+        
