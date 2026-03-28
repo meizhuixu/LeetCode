9218b4a4540f371e95302d1942a527354fc457bd
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-27 23:04:16 -0700

    Time: 44 ms (69.65%), Space: 19.4 MB (5.66%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        mark = 1 if x > 0 else -1
+        x = abs(x)
+
+        res = 0
+        while x != 0:
+            temp = x % 10
+            res = res * 10 + temp
+            if res >= 2 ** 31 or res < - 2 ** 31:
+                return 0
+            x //= 10
+
+        return res * mark
