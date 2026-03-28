4cb8917e51dc897dc1e1dbc9fbac440c9dd03009
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-28 15:28:16 -0700

    Time: 0 ms (100%), Space: 19.4 MB (35.31%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,18 +1,24 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        # binary search
+        def can_split(max_sum):
+            count, total = 1, 0
+            for num in nums:
+                total += num
+                if total > max_sum:
+                    count += 1
+                    if count > k:
+                        return False
+
+                    total = num
+            return True
+
+
         l, r = max(nums), sum(nums)
 
         while l < r:
             mid = (l + r) // 2
-            count, total = 1, 0
-            for num in nums:
-                total += num
-                if total > mid:
-                    count += 1
-                    total = num
-
-            if count <= k:
+            
+            if can_split(mid):
                 r = mid
             else:
                 l = mid + 1
