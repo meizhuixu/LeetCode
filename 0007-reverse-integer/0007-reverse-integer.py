5794512ab7744e0c1bb46585b0f54502fac5c166
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-27 23:08:15 -0700

    Time: 51 ms (25.17%), Space: 19.2 MB (54.91%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -7,7 +7,7 @@ class Solution:
         while x != 0:
             temp = x % 10
             res = res * 10 + temp
-            if res >= 2 ** 31 or res < - 2 ** 31:
+            if res >= 2 ** 31:
                 return 0
             x //= 10
 
