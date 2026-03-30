db02474448db7a41ba923302ba34f0f1996ae107
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-30 11:38:22 -0700

    Time: 0 ms (100%), Space: 19.3 MB (98.6%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -15,19 +15,20 @@ class Solution:
         while queue:
             
             length = len(queue)
-            level = []
+            level = deque()
             for _ in range(length):
                 cur = queue.popleft()
-                level.append(cur.val)
+                if l_to_r:
+                    level.append(cur.val)
+                else:
+                    level.appendleft(cur.val)
 
                 if cur.left:
                     queue.append(cur.left)
                 if cur.right:
                     queue.append(cur.right)
 
-            if not l_to_r:
-                level.reverse()
-            res.append(level)
+            res.append(list(level))
             l_to_r = not l_to_r
 
         return res
