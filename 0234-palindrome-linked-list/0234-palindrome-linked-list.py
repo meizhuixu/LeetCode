ad465bb14d870c1f17357d9cebb6fbbc5ad31d96
Meizhui Xu <xu.meiz@northeastern.edu>
2026-03-31 13:58:16 -0700

    Time: 28 ms (74.92%), Space: 53.5 MB (34.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,11 +5,29 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nums = []
-        cur = head
-        while cur:
-            nums.append(cur.val)
-            cur = cur.next
+        # find cut point
+        slow = fast = dummy = ListNode(next = head)
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        return nums == nums[::-1]
+        # reverse the right half
+        if slow.next and slow.next.next:
+            pre, cur, nxt = slow, slow.next.next, slow.next
+            nxt.next = None
+            while cur:
+                temp = cur.next
+                pre.next = cur
+                cur.next = nxt
+                cur, nxt = temp, cur
+
+        p1, p2 = head, slow.next
+        while p1 and p2:
+            if p1.val != p2.val:
+                return False
+            p1, p2 = p1.next, p2.next
+
+        return True
+
+        
         
