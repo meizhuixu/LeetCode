13fec1a9f4ab45df4da9135697074383b82b4537
Meizhui Xu <xu.meiz@northeastern.edu>
2026-04-15 17:26:06 -0700

    Time: 258 ms (56.34%), Space: 20.4 MB (71.84%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,27 +1,37 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # bfs -> shortest path
+        # hit -> *it h*t hi* -> in wordlist
+        # wordlist -> wordset  O(1)
+        # find endword -> return step
+        # return 0: 1. endword not in wordlist  2. during search -> no new word in wordlist
+        # time: O(n * n * m)     n = len(word)   m = len(wordlist)
+        # space: O(n * m)
+
         wordSet = set(wordList)
+        # pruning
         if endWord not in wordSet:
             return 0
 
+        path = set([beginWord])
         queue = deque([(beginWord, 1)])
-        visited = set([beginWord])
         while queue:
-            word, count = queue.popleft()
-
+            word, step = queue.popleft()
             for i in range(len(word)):
                 word_list = list(word)
-
                 for j in range(26):
                     word_list[i] = chr(ord('a') + j)
-                    new_word = ''.join(word_list)
-
+                    new_word = ''.join(word_list)  # O(n)
+                    if new_word in path:
+                        continue
                     if new_word == endWord:
-                        return count + 1
-                    if new_word not in visited and new_word in wordSet:
-                        visited.add(new_word)
-                        queue.append((new_word, count + 1))
+                        return step + 1
+                    if new_word in wordSet:
+                        path.add(new_word)
+                        queue.append((new_word, step + 1))
 
         return 0
+
+
 
         
