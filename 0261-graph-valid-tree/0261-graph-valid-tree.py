434bbb78c6d8707746dc1f60a941b5caef715aa1
Meizhui Xu <xu.meiz@northeastern.edu>
2026-04-19 21:27:48 -0700

    Time: 1 ms (50.38%), Space: 20.7 MB (27.96%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,6 +1,7 @@
 class UnionFind:
     def __init__(self, n):
         self.parent = [i for i in range(n)]
+        self.edges = 0
 
     def find(self, u):
         if self.parent[u] == u:
@@ -8,29 +9,32 @@ class UnionFind:
         self.parent[u] = self.find(self.parent[u])
         return self.parent[u]
 
+    def join(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        if u != v:
+            self.parent[u] = v
+            self.edges += 1
+
     def isSame(self, u, v):
         u = self.find(u)
         v = self.find(v)
         return u == v
 
-    def join(self, u, v):
-        u = self.find(u)
-        v = self.find(v)
-        if u == v:
-            return
-        self.parent[u] = v
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n - 1:
-            return False
+        # valid tree: 
+        #1. no circle   
+        #2. all nodes connected
+        # nodes = n, edges = n - 1
 
+        # union find:
+        # 1. check if there is a circle: return False
+        # 2. check if all nodes are connected
         uf = UnionFind(n)
         for u, v in edges:
             if uf.isSame(u, v):
                 return False
             uf.join(u, v)
 
-        return True
-
-        
+        return uf.edges == n - 1
