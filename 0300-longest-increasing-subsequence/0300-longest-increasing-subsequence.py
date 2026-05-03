014167130b24cc4e8fc20b9fd80943669dccbb37
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-03 14:40:45 -0700

    Time: 7 ms (82.37%), Space: 19.3 MB (85.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,23 +1,25 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         def find_insert_index(num):
-            l, r = 0, len(dp)
+            l, r = 0, len(LIS)
             while l < r:
                 mid = (l + r) // 2
-                if dp[mid] >= num:
+                if LIS[mid] >= num:
                     r = mid
                 else:
                     l = mid + 1
 
             return l
 
-        dp = [nums[0]]
+        LIS = [nums[0]]
+        res = 1
         for i in range(1, len(nums)):
-            if nums[i] > dp[-1]:
-                dp.append(nums[i])
+            if nums[i] > LIS[-1]:
+                LIS.append(nums[i])
+                res += 1
             else:
                 idx = find_insert_index(nums[i])
-                dp[idx] = nums[i]
+                LIS[idx] = nums[i]
 
-        return len(dp)
+        return res
         
