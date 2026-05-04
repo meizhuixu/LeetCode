78ee695eabfb93a6d0987bf6aeee875ea84672bd
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-03 20:03:12 -0700

    Time: 0 ms (100%), Space: 19 MB (99.01%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,17 +4,19 @@ class Solution:
         if n == 1:
             return nums[0]
 
-        pre1 = cur1 = 0
+        pre = cur1 = 0
         for i in range(n - 1):
             temp = cur1
-            cur1 = max(nums[i] + pre1, cur1)
-            pre1 = temp
+            cur1 = max(nums[i] + pre, cur1)
+            pre = temp
 
-        pre2 = cur2 = 0
-        for j in range(1, n):
+        pre = cur2 = 0
+        for i in range(1, n):
             temp = cur2
-            cur2 = max(nums[j] + pre2, cur2)
-            pre2 = temp
+            cur2 = max(nums[i] + pre, cur2)
+            pre = temp
 
         return max(cur1, cur2)
+
+        
         
