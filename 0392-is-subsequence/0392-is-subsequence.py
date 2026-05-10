66706377ec80682616dc34b88196ec9b73086c8c
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-09 23:09:41 -0700

    Time: 19 ms (3.43%), Space: 19.9 MB (7.51%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -10,6 +10,6 @@ class Solution:
                 if s[i - 1] == t[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    dp[i][j] = dp[i][j - 1]
 
         return dp[-1][-1] == m
