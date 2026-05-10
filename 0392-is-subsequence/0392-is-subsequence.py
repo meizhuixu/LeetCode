eb9d88e2a8a97a8bf3f80baf1ffbe47517c8fe36
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-09 23:12:34 -0700

    Time: 16 ms (3.43%), Space: 19.3 MB (26.39%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,12 +4,15 @@ class Solution:
         if m > n:
             return False
 
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp = [0] * (n + 1)
         for i in range(1, m + 1):
+            prev = 0
             for j in range(1, n + 1):
+                curr = dp[j]
                 if s[i - 1] == t[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    dp[j] = prev + 1
                 else:
-                    dp[i][j] = dp[i][j - 1]
+                    dp[j] = dp[j - 1]
+                prev = curr
 
-        return dp[-1][-1] == m
+        return dp[-1] == m
