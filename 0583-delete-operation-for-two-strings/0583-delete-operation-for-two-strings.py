5043faf1042265dbd85352533b492665c96b6a7e
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-12 18:24:46 -0700

    Time: 75 ms (77.29%), Space: 19.3 MB (96.15%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -6,19 +6,20 @@ class Solution:
         # dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1
 
         m, n = len(word1), len(word2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp = [i for i in range(n + 1)]
 
-        for i in range(m + 1):
+        for i in range(1, m + 1):
+            prev = i - 1
             for j in range(n + 1):
-                if i == 0:
-                    dp[i][j] = j
-                elif j == 0:
-                    dp[i][j] = i
+                if j == 0:
+                    dp[j] = i
                 else:
+                    curr = dp[j]
                     if word1[i - 1] == word2[j - 1]:
-                        dp[i][j] = dp[i - 1][j - 1]
+                        dp[j] = prev
                     else:
-                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1
+                        dp[j] = min(dp[j], dp[j - 1]) + 1
+                    prev = curr
 
-        return dp[-1][-1]
+        return dp[-1]
         
