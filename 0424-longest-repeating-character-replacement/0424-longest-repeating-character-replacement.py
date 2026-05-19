d2b56bc834aff1f94f1ea92b1e77e4483f213ee4
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-19 08:44:52 +0800

    Time: 75 ms (88.92%), Space: 19.5 MB (85.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,28 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l = res = most = 0
-        seen = defaultdict(int)
+        # time: O(n)
+        # space: O(1)   26
+        # s = "AABABBA", k = 1
+        #          l r
+        # hashmap = {'A': 1, 'B': 2 }
+        # res = 4
+
+        # "BAAAB"  k = 2
+        # hashmap = {'A':3 , 'B': 2 }
+
+        hashmap = defaultdict(int)
+        res = l = count = 0
 
         for r in range(len(s)):
-            seen[s[r]] += 1
-            most = max(most, seen[s[r]])
-            
-            if (r - l + 1) - most > k:
-                seen[s[l]] -= 1
+            char = s[r]
+            hashmap[char] += 1
+            count = max(count, hashmap[char])
+            while r - l + 1 - count > k:
+                drop = s[l]
+                hashmap[drop] -= 1
                 l += 1
 
             res = max(res, r - l + 1)
 
         return res
-
         
