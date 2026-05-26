111e226771324f26ca381808205355bd868b3a39
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-26 14:35:59 +0800

    Time: 112 ms (13.5%), Space: 26 MB (15.25%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,16 +1,17 @@
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
+        # time O(E); space O(V + E)
         graph = defaultdict(list)
         for u, v, w in connections:
             graph[u].append((v, w))
             graph[v].append((u, w))
 
-        visited = [False] * (n + 1)
+        visited = [False] * (n + 1) # space O(V)
         total_cost = conneted_city = 0
         pq = [(0, 1)]
 
         while pq:
-            cost, city = heapq.heappop(pq)
+            cost, city = heapq.heappop(pq) # time O(ElogV)
 
             if visited[city]:
                 continue
@@ -19,7 +20,7 @@ class Solution:
             total_cost += cost
             conneted_city += 1
 
-            for nei, wei in graph[city]:
+            for nei, wei in graph[city]: # time O(ElogV)
                 heapq.heappush(pq, (wei, nei))
 
         return total_cost if conneted_city == n else -1
