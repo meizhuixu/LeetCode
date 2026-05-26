9e9a6d588684e7510ca41500508edd63408c7d8f
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-27 06:04:58 +0800

    Time: 36 ms (97.3%), Space: 23.6 MB (98.28%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,28 +1,35 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.count = 1
+
+    def find(self, u):
+        if self.parent[u] == u:
+            return u
+        self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def join(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        if u == v:
+            return False
+        else:
+            self.parent[u] = v
+            self.count += 1
+            return True
+
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
-        # time O(E); space O(V + E)
-        graph = defaultdict(list)
-        for u, v, w in connections:
-            graph[u].append((v, w))
-            graph[v].append((u, w))
+        connections.sort(key=lambda x: x[2])
+        uf = UnionFind(n)
+        total_cost = 0
 
-        visited = [False] * (n + 1) # space O(V)
-        total_cost = conneted_city = 0
-        pq = [(0, 1)]
+        for x, y, cost in connections:
+            if uf.join(x, y):
+                total_cost += cost
 
-        while pq:
-            cost, city = heapq.heappop(pq) # time O(ElogV)
+        return total_cost if uf.count == n else -1
 
-            if visited[city]:
-                continue
-
-            visited[city] = True
-            total_cost += cost
-            conneted_city += 1
-
-            for nei, wei in graph[city]: # time O(ElogV)
-                heapq.heappush(pq, (wei, nei))
-
-        return total_cost if conneted_city == n else -1
 
         
