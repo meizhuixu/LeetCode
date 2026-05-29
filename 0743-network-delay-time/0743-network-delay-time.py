d011727c32b1103f7d1a2a12577d4ef9a4d69f88
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-28 20:28:33 -0700

    Time: 351 ms (29.34%), Space: 22 MB (8.49%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,23 +1,32 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # time: O(E); space: O(V + E)
+        # build graph
+        # dist = [w]    dist to reach node i is dist[i] time
+        # pq [(0, k)]  (node, the minimum time to reach node)
+
+        # times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
         graph = defaultdict(list)
         for u, v, w in times:
-            graph[u].append((v, w))
+            graph[u].append((w, v))
 
-        visited = {}
-        pq = [(0, k)]
+        dist = [float('inf')] * (n + 1)   # [++, 1, 0]
+        pq = [(0, k)] # 
 
-        # time: O(ElogV)
         while pq:
             time, node = heapq.heappop(pq)
 
-            if node in visited:
+            if time > dist[node]:
                 continue
 
-            visited[node] = time
-            for nxt, dt in graph[node]:
-                heapq.heappush(pq, (time + dt, nxt))
-                
-        # time: O(V)
-        return max(visited.values()) if len(visited) == n else -1
+            dist[node] = time
+            for dt, nxt in graph[node]:
+                if dist[nxt] > time + dt:
+                    heapq.heappush(pq, (time + dt, nxt))
+
+        res = max(dist[i] for i in range(1, n + 1))
+        print(dist)
+        return res if res != float('inf') else -1
+        
+
+
+        
