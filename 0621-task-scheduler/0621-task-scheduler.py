f53f3a4435367b6bef5a2444e93a9e39930e14bc
Meizhui Xu <xu.meiz@northeastern.edu>
2026-05-30 13:30:26 -0700

    Time: 165 ms (37.15%), Space: 20.9 MB (15.23%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,13 +1,29 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # greedy
-        hashmap = Counter(tasks)
-        max_freq = [0, 0] # freq, number of tasks
-        for freq in hashmap.values():
-            if freq == max_freq[0]:
-                max_freq[1] += 1
-            elif freq > max_freq[0]:
-                max_freq = [freq, 1]
+        # simulation/pq
+        # pq = [-3, -3]   pop the most freq task
+        # queue = [(-2, 4), (-2, 5)]  FIFO the first task back to pq
 
-        res = max_freq[0] * (n + 1) - n + (max_freq[1] - 1)
-        return max(len(tasks), res)
+        hashmap = Counter(tasks)
+        pq = []
+        for freq in hashmap.values():
+            heapq.heappush(pq, -freq)
+
+        queue = deque()
+        time = 0
+        while pq or queue:
+            time += 1
+            if pq:
+                neg_freq = heapq.heappop(pq)
+                if neg_freq < -1:
+                    queue.append((neg_freq + 1, time + n))
+
+            if queue and queue[0][1] == time:
+                neg_freq, _ = queue.popleft()
+                heapq.heappush(pq, neg_freq)
+
+        return time
+
+
+    
+        
