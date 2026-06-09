9530bfea4a298aaf174134a12cce640a05a230ce
Meizhui Xu <xu.meiz@northeastern.edu>
2026-06-08 17:58:40 -0700

    Time: 3 ms (40.64%), Space: 19.4 MB (36.34%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,47 +1,58 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        # initiate graph and in_degree
+        # words = ["wrt","wrf","er","ett","rftt"]
+        # t: f
+        # w: e
+        # r: t
+        # e: r
+
+
         graph = {}
+        indegree = {}
+
         for word in words:
             for chr in word:
                 graph[chr] = set()
+                indegree[chr] = 0
 
-        in_degree = {}
-        for chr in graph:
-            in_degree[chr] = 0
-
-        # build graph and in_degree
         for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            min_len = min(len(w1), len(w2))
-            # 'abb' and 'ab' are ineligible
-            if len(w1) > len(w2) and w1[:min_len] == w2:
-                return ''
-
-            for j in range(min_len):
-                if w1[j] != w2[j]:
-                    if w2[j] not in graph[w1[j]]:
-                        graph[w1[j]].add(w2[j])
-                        in_degree[w2[j]] += 1
+            j = 0
+            while j < min(len(words[i]), len(words[i + 1])):
+                min_len = min(len(words[i]), len(words[i + 1]))
+                if len(words[i]) > len(words[i + 1]) and words[i][:min_len] == words[i + 1]:
+                    return ''
+                if words[i][j] != words[i + 1][j]:
+                    if words[i + 1][j] not in graph[words[i][j]]:
+                        graph[words[i][j]].add(words[i + 1][j])
+                        indegree[words[i + 1][j]] += 1
                     break
+                else:
+                    j += 1
+        print(graph)
+        print(indegree)
 
-        # put all zero indegree character into queue
         queue = deque()
-        for chr, freq in in_degree.items():
-            if freq == 0:
-                queue.append(chr)
+        for key in graph:
+            if indegree[key] == 0:
+                queue.append(key)
 
         res = []
         while queue:
-            cur = queue.popleft()
-            res.append(cur)
+            curr = queue.popleft()
+            res.append(curr)
 
-            for nei in graph[cur]:
-                in_degree[nei] -= 1
-                if in_degree[nei] == 0:
+            for nei in graph[curr]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
                     queue.append(nei)
 
         return ''.join(res) if len(res) == len(graph) else ''
 
+        
 
-            
+
+
+
+
+
+        
