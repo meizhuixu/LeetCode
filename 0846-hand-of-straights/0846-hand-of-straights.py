80de30dca73842ab511f5f51e64eb91203912403
Meizhui Xu <xu.meiz@northeastern.edu>
2026-06-10 00:11:48 -0700

    Time: 25 ms (92.18%), Space: 21.2 MB (43.58%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,30 +1,25 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        # check its mod -> False
-        # sort
-        # 1, 2, 2, 3, 3, 4, 6, 7, 8
-        # Counter
-        # {1: 1, 2: 2, 3: 2, 4: 1, 6: 1, 7: 1, 8: 1}
-
-        if len(hand) % groupSize != 0:
+        if len(hand) % groupSize > 0:
             return False
 
         hashmap = Counter(hand)
-        for num in hand: # 4
-            start = num  
-            while hashmap[start - 1] > 0: # find start
-                start -= 1  # 1
-
-            while start in hashmap and start <= num:
-                while hashmap[start] > 0:
-                    temp = start
-                    for _ in range(groupSize):
-                        if hashmap[temp] > 0:
-                            hashmap[temp] -= 1
-                            temp += 1
-                        else:
+        for card in hand:
+            # pruning
+            if hashmap[card] == 0:
+                continue
+            
+            start = card
+            while hashmap[start - 1] > 0:
+                start -= 1
+            
+            while start <= card:
+                count = hashmap[start]
+                if count > 0:
+                    for i in range(start, start + groupSize):
+                        if hashmap[i] < count:
                             return False
+                        hashmap[i] -= count
                 start += 1
 
-        
         return True
