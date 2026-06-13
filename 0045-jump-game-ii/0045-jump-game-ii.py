766f100574a2266d7874d7f613908719eb1830e0
Meizhui Xu <xu.meiz@northeastern.edu>
2026-06-12 22:33:50 -0700

    Time: 7 ms (54.26%), Space: 20 MB (66.85%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+
+        res = i = curr = nxt = 0
+        for i in range(n - 1):
+            nxt = max(nxt, i + nums[i])
+            if nxt >= n - 1:
+                return res + 1
+            if i == curr:
+                res += 1
+                curr = nxt       
