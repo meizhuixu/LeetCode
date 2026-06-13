f2211dd932b748f14c01b4039c1f38cd8b7099e9
Meizhui Xu <xu.meiz@northeastern.edu>
2026-06-12 22:34:09 -0700

    Time: 7 ms (54.26%), Space: 20.3 MB (16.48%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,7 +4,7 @@ class Solution:
         if n == 1:
             return 0
 
-        res = i = curr = nxt = 0
+        res = curr = nxt = 0
         for i in range(n - 1):
             nxt = max(nxt, i + nums[i])
             if nxt >= n - 1:
