545ee76d1a8690ee39c1fbde26ad0063822a6b3c
Meizhui Xu <xu.meiz@northeastern.edu>
2026-06-18 21:55:56 -0700

    Time: 0 ms (100%), Space: 19.5 MB (39.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,31 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        #  nums = [0,1,2,4,5,6,7]
-        #  nums = [4,5,6,7,0,1,2]
-        #          l        m  r
-        #  nums = [4,5,6,7,8,9,0,1,2]
-        #                  ml      r
-        
-        # time: O(logn) n = len(nums)
-        # space: O(1)
-        
-        l, r = 0, len(nums) - 1
-        
-        while l <= r:
-            mid = (l + r) // 2
-            
+        # [4,5,6,7,0,1,2]
+        #  l           r
+        # [0,1,2,4,5,6,7]
+        #  l           r
+        # binary search
+        n = len(nums)
+        l, r = 0, n - 1
+
+        while l <= r:   # 0, 1
+            mid = (l + r) // 2   # 0
             if nums[mid] == target:
                 return mid
-            elif nums[mid] >= nums[l]:
-                if nums[l] <= target < nums[mid]:
-                    r = mid - 1
-                else:
-                    l = mid + 1   
-            elif nums[mid] < nums[l]:
+
+            if nums[mid] < nums[r]:
                 if nums[mid] < target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid - 1
-                    
+            elif nums[mid] >= nums[r]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
         return -1
+
+
         
