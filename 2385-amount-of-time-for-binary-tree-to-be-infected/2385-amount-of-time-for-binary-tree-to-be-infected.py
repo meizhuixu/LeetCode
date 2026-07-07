8ac33d3a28d7d6d52518cab80b461e65ae258bc0
Meizhui Xu <xu.meiz@northeastern.edu>
2026-07-06 23:20:42 -0700

    Time: 173 ms (50.67%), Space: 101 MB (30.59%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        # build graph
+        hashmap = defaultdict(list)
+        visited = set()
+
+        def build(node):
+
+            if node.right:
+                hashmap[node.val].append(node.right.val)
+                hashmap[node.right.val].append(node.val)
+                build(node.right)
+
+            if node.left:
+                hashmap[node.val].append(node.left.val)
+                hashmap[node.left.val].append(node.val)
+                build(node.left)
+
+        build(root)
+
+        # infection
+        queue = deque()
+        for nei in hashmap[start]:
+            queue.append(nei)
+
+        res = 0
+        visited = set([start])
+        while queue:
+            length = len(queue)
+            res += 1
+            for _ in range(length):
+                cur = queue.popleft()
+                visited.add(cur)
+
+                for nei in hashmap[cur]:
+                    if nei not in visited:
+                        queue.append(nei)
+
+        return res
+
+
+
+
+            
+
+
