31b626016aafe96d91f9e457ff68dca196d9185a
Meizhui Xu <xu.meiz@northeastern.edu>
2026-07-20 19:41:49 -0700

    Time: 3 ms (84.23%), Space: 22.2 MB (14.11%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,12 +1,17 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         hashmap = defaultdict(int)
+
         for s, e in intervals:
             hashmap[s] += 1
             hashmap[e] -= 1
 
-        count = res = 0
-        for t in sorted(hashmap.keys()):
-            count += hashmap[t]
-            res = max(res, count)
+        res = cnt = 0
+        for time in sorted(hashmap.keys()):
+            cnt += hashmap[time]
+            res = max(res, cnt)
+
         return res
+
+
+        
