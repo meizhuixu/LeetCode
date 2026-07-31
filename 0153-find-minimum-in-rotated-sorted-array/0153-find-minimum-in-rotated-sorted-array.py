76ea5b20a1cf3cca64578231a3a0b2e534eb72e5
Meizhui Xu <xu.meiz@northeastern.edu>
2026-07-30 22:10:41 -0700

    Time: 0 ms (100%), Space: 19.4 MB (24.43%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -10,11 +10,12 @@ class Solution:
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] <= nums[-1]:
+                res = mid
                 r = mid - 1
             else:
                 l = mid + 1
 
-        return nums[l]
+        return nums[res]
         
 
         
