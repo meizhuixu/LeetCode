c84549d7379c8d7e3dffc3aaaf61d2e27bc1b68d
Meizhui Xu <xu.meiz@northeastern.edu>
2026-08-17 21:32:09 -0700

    Time: 0 ms (100%), Space: 21.4 MB (57.06%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,13 +1,14 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n, i = len(intervals), 0
-        s, e = newInterval
         res = []
+        i, n = 0, len(intervals)
+        s, e = newInterval
 
         while i < n and intervals[i][1] < s:
             res.append(intervals[i])
             i += 1
 
+        
         while i < n and intervals[i][0] <= e:
             s = min(intervals[i][0], s)
             e = max(intervals[i][1], e)
@@ -19,7 +20,13 @@ class Solution:
             i += 1
 
         return res
+        
 
 
 
 
+
+
+
+
+        
