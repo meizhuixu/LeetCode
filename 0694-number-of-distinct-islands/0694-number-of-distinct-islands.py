df3c62c29f98fc463a192cb9acb781e32bbe2afd
Meizhui Xu <xu.meiz@northeastern.edu>
2026-08-30 12:31:58 -0700

    Time: 53 ms (18.72%), Space: 21.1 MB (33.97%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -1,23 +1,33 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
-        def dfs(x, y, path):
-            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == 0: 
-                return
-
-            grid[x][y] = 0
-            for direction, pair in {'u': (0, 1), 'd': (0, -1), 'r': (1, 0), 'l': (-1, 0)}.items():
-                dx, dy = pair
-                path.append(direction)
-                dfs(x + dx, y + dy, path)
+        # 1. iterate through matrix
+        # 2. meet 1: dfs， set 1 to 0
+        # store each relative coordinate of adjacent 1
+        # 3. return length of res set
 
         m, n = len(grid), len(grid[0])
-        res = set()
+        set_res = set()
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+        def dfs(x, y, ox, oy):
+            grid[x][y] = 0
+            set_island.add((x-ox, y-oy))
+
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and (nx-ox, ny-oy) not in set_island and grid[nx][ny] == 1:
+                    dfs(nx, ny, ox, oy)
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    path = []
-                    dfs(i, j, path)
-                    res.add(''.join(path))
-                
-        return len(res)
+                    set_island = set()
+                    dfs(i, j, i, j)
+                    print(set_island)
+                    set_res.add(tuple(set_island))
+
+        return len(set_res)
+
+
+
         
