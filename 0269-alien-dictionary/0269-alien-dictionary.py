5e3b9a4b78f1fd68a0b73d933ef41c6b98851c25
Meizhui Xu <xu.meiz@northeastern.edu>
2026-08-30 22:40:20 -0700

    Time: 0 ms (100%), Space: 19.5 MB (36.74%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,41 +1,57 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        graph = {}
-        indegree = {}
-        for word in words:
-            for chr in word:
-                graph[chr] = set()
-                indegree[chr] = 0
+        # topologic
+        # 1. build graph: compare each adjacent 2 words
+        # 2. topologic: is circle return empty str
         
+        word_set = set()
+        for word in words:
+            for char in word:
+                word_set.add(char)
 
-        for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            min_len = min(len(w1), len(w2))
-
-            if len(w1) > len(w2) and w1[:min_len] == w2:
+        n = len(words)
+        graph = defaultdict(set)
+        indegree = defaultdict(int)
+        for i in range(n - 1):
+            length = min(len(words[i]), len(words[i + 1]))
+            if words[i][:length] == words[i+1][:length] and len(words[i]) > length:
                 return ''
 
-            for j in range(min_len):
-                if w1[j] != w2[j]:
-                    if w2[j] not in graph[w1[j]]:
-                        graph[w1[j]].add(w2[j])
-                        indegree[w2[j]] += 1
-                    break
-        print(indegree)
+            for j in range(length):
+                if words[i][j] == words[i+1][j]:
+                    continue
+                if words[i+1][j] not in graph[words[i][j]]:
+                    graph[words[i][j]].add(words[i+1][j])
+                    indegree[words[i+1][j]] += 1
+                break
 
         queue = deque()
-        for chr, num in indegree.items():
-            if num == 0:
-                queue.append(chr)
+        for char in word_set:
+            if indegree[char] == 0:
+                queue.append(char)
 
         res = []
         while queue:
-            curr = queue.popleft()
-            res.append(curr)
+            cur = queue.popleft()
+            res.append(cur)
+            
+            for nxt in graph[cur]:
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0:
+                    queue.append(nxt)
 
-            for nei in graph[curr]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    queue.append(nei)
+        print(graph)
 
-        return ''.join(res) if len(res) == len(indegree) else ''
+        return '' if len(res) < len(word_set) else ''.join(res)
+
+            
+
+        
+
+        
+
+
+
+                
+                
+        
